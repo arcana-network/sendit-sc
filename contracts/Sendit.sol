f8dcfdb47d71475f638514ecbf44ccd946987b9e
SauravKanchan <sauravnk30@gmail.com>
2023-10-04 23:20:33 +0530

    format code

--- contracts/Sendit.sol
+++ contracts/Sendit.sol
@@ -5,24 +5,36 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
 // import "hardhat/console.sol";
 
-contract Sendit is EIP712Upgradeable,ReentrancyGuardUpgradeable  {
-  enum RequestStatus { OPEN, REJECTED, COMPLETED }
+contract Sendit is EIP712Upgradeable, ReentrancyGuardUpgradeable {
+  enum RequestStatus {
+    OPEN,
+    REJECTED,
+    COMPLETED
+  }
   struct Request {
-      uint256 nonce;
-      address recipient;
-      uint256 value;
-      address token_address;
-      uint256 expiry;
-      RequestStatus status;
+    uint256 nonce;
+    address recipient;
+    uint256 value;
+    address token_address;
+    uint256 expiry;
+    RequestStatus status;
   }
   mapping(address => mapping(uint256 => bool)) public nonces;
   mapping(address => mapping(uint256 => Request)) public requests;
-  event RequestCompleted(uint256 nonce, address recipient, uint256 value, address token_address);
+  event RequestCompleted(
+    uint256 nonce,
+    address recipient,
+    uint256 value,
+    address token_address
+  );
 
-  bytes32 private constant REQUEST_TYPEHASH = 
-  keccak256("Request(uint256 chainId,uint256 nonce,address recipient,uint256 value,address token_address,uint256 expiry)");
+  bytes32 private constant REQUEST_TYPEHASH =
+    keccak256(
+      "Request(uint256 chainId,uint256 nonce,address recipient,uint256 value,address token_address,uint256 expiry)"
+    );
 
   function initialize() public initializer {
     __ReentrancyGuard_init();
@@ -36,8 +48,16 @@ contract Sendit is EIP712Upgradeable,ReentrancyGuardUpgradeable  {
   // @param _token_address the address of the token to be used for the payment
   // @param _signature the signature of the request parameters
   // @param _expiry the expiry of the request
-  function send(uint256 _nonce,address _recipient, uint256 _value, address _token_address, uint256 _expiry,
-  uint8 v, bytes32 r, bytes32 s) nonReentrant external payable {
+  function send(
+    uint256 _nonce,
+    address _recipient,
+    uint256 _value,
+    address _token_address,
+    uint256 _expiry,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) external payable nonReentrant {
     // check if the request is valid
     require(nonces[_recipient][_nonce] == false, "Sendit: Nonce already used");
     require(_recipient != address(0), "Sendit: invalid recipient");
@@ -45,17 +65,45 @@ contract Sendit is EIP712Upgradeable,ReentrancyGuardUpgradeable  {
     // check if the request is not expired
     require(_expiry > block.timestamp, "Sendit: request is expired");
     // check if the request is not already completed
-    require(requests[_recipient][_nonce].status != RequestStatus.COMPLETED, "Sendit: request is completed");
+    require(
+      requests[_recipient][_nonce].status != RequestStatus.COMPLETED,
+      "Sendit: request is completed"
+    );
     // check if the request is not already rejected
-    require(requests[_recipient][_nonce].status != RequestStatus.REJECTED, "Sendit: request is rejected");
-    // request should be open 
-    require(requests[_recipient][_nonce].status == RequestStatus.OPEN, "Sendit: request is not open");
+    require(
+      requests[_recipient][_nonce].status != RequestStatus.REJECTED,
+      "Sendit: request is rejected"
+    );
+    // request should be open
+    require(
+      requests[_recipient][_nonce].status == RequestStatus.OPEN,
+      "Sendit: request is not open"
+    );
     // check if the signature is valid
-    bytes32 digest = _hashTypedDataV4(keccak256(abi.encode(REQUEST_TYPEHASH, block.chainid,_nonce,_recipient, _value, _token_address, _expiry)));
+    bytes32 digest = _hashTypedDataV4(
+      keccak256(
+        abi.encode(
+          REQUEST_TYPEHASH,
+          block.chainid,
+          _nonce,
+          _recipient,
+          _value,
+          _token_address,
+          _expiry
+        )
+      )
+    );
     address signer = ECDSA.recover(digest, v, r, s);
     require(signer == _recipient, "Sendit: invalid signature");
-     // update the request status
-    requests[_recipient][_nonce] = Request(_nonce, _recipient, _value, _token_address, _expiry,RequestStatus.COMPLETED);
+    // update the request status
+    requests[_recipient][_nonce] = Request(
+      _nonce,
+      _recipient,
+      _value,
+      _token_address,
+      _expiry,
+      RequestStatus.COMPLETED
+    );
     nonces[_recipient][_nonce] = true;
 
     // transfer the payment
@@ -71,7 +119,7 @@ contract Sendit is EIP712Upgradeable,ReentrancyGuardUpgradeable  {
       bool sent = token.transferFrom(msg.sender, _recipient, _value);
       require(sent, "Sendit: failed to send tokens");
     }
-   
+
     emit RequestCompleted(_nonce, _recipient, _value, _token_address);
   }
 }
